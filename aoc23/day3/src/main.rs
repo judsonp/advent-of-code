df8f41fb438aaecf20576793006c189f8595a319
Judson Powers <judson.powers@gmail.com>
2023-12-03 10:33:02 -0500

    Day 3, Part 2

--- aoc23/day3/src/main.rs
+++ aoc23/day3/src/main.rs
@@ -21,14 +21,19 @@ impl Span {
         ul.x <= p.x && p.x <= br.x && ul.y <= p.y && p.y <= br.y
     }
 
-    fn within_points(&self, ps: &Vec<Point>) -> bool {
-        ps.iter().any(|p| self.within_point(p))
+    fn within_symbols(&self, ps: &Vec<Symbol>) -> bool {
+        ps.iter().any(|s| self.within_point(&s.loc))
     }
 }
 
 struct Number {
     n: u64,
     loc: Span,
+}
+
+struct Symbol {
+    is_gear: bool,
+    loc: Point,
 }
 
 fn main() {
@@ -46,14 +51,35 @@ fn main() {
             }
         }).collect();
 
-    let symbols: Vec<Point> = input.split("\n").enumerate()
+    let symbols: Vec<Symbol> = input.split("\n").enumerate()
         .flat_map(|(line_nr, line)| symbol_re.find_iter(line).map(move |m| (line_nr, m)))
-        .map(|(line_nr, sym_match)| Point { x: sym_match.start(), y: line_nr })
+        .map(|(line_nr, sym_match)| {
+            Symbol {
+                is_gear: sym_match.as_str().contains('*'),
+                loc: Point { x: sym_match.start(), y: line_nr },
+            }
+        })
         .collect();
 
-    let r: u64 = numbers.iter()
-        .filter(|n| n.loc.within_points(&symbols))
+    println!("Part 1: {}", part_one(&numbers, &symbols));
+    println!("Part 2: {}", part_two(&numbers, &symbols));
+}
+
+fn part_one(numbers: &Vec<Number>, symbols: &Vec<Symbol>) -> u64 {
+    numbers.iter()
+        .filter(|n| n.loc.within_symbols(&symbols))
         .map(|n| n.n)
-        .sum();
-    println!("{}", r);
+        .sum()
+}
+
+fn part_two(numbers: &Vec<Number>, symbols: &Vec<Symbol>) -> u64 {
+    symbols.iter().filter(|s| s.is_gear)
+        .filter_map(|s| {
+            let (count, gear_ratio) = numbers.iter()
+                .filter(|n| n.loc.within_point(&s.loc))
+                .map(|s| (1, s.n))
+                .fold((0, 1), |(ac, ap), (c, p)| (ac + c, ap * p));
+            if count == 2 { Some(gear_ratio) } else { None }
+        })
+        .sum()
 }
