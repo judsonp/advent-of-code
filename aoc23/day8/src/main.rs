ecf4319ab21afe873c4d2b78087517d76a502f90
Judson Powers <judson.powers@gmail.com>
2023-12-08 15:34:28 -0500

    Day 8, Part 2

--- aoc23/day8/src/main.rs
+++ aoc23/day8/src/main.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 use std::fs;
-use nom::bytes::complete::{is_a, tag};
+use num::integer::lcm;
+use nom::bytes::complete::tag;
 use nom::character::complete::{alpha1, line_ending, multispace0, one_of, space0};
 use nom::combinator::{complete, map};
-use nom::{InputIter, IResult};
+use nom::IResult;
 use nom::multi::{many1, separated_list1};
 use nom::sequence::{delimited, separated_pair, terminated};
 
@@ -31,21 +32,64 @@ fn main() {
     let input_s = fs::read_to_string("input.txt").unwrap();
     let (_, input) = parse_input(&input_s).unwrap();
     println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
 }
 
 fn part_one(input: &Input) -> usize {
-    let mut cur_nodelabel = "AAA";
+    let mut current_node = "AAA";
     let mut count: usize = 0;
-    while !cur_nodelabel.eq("ZZZ") {
-        let node = input.graph.get(cur_nodelabel).unwrap();
+    while !current_node.eq("ZZZ") {
         let direction = &input.directions[count % input.directions.len()];
-        cur_nodelabel = match direction {
-            Direction::Left => &node.left,
-            Direction::Right => &node.right,
-        };
+        current_node = advance_node(input, current_node, direction);
         count += 1;
     }
     return count;
+}
+
+fn advance_node<'a>(input: &'a Input, current: &str, direction: &Direction) -> &'a str {
+    let node = input.graph.get(current).unwrap();
+    return match direction {
+        Direction::Left => &node.left,
+        Direction::Right => &node.right,
+    };
+}
+
+fn part_two(input: &Input) -> usize {
+    let start_nodes: Vec<&str> = input.graph.keys()
+        .filter(|label| label.ends_with("A"))
+        .map(|s| -> &str { s }).collect();
+    let mut path_lengths: Vec<usize> = Vec::new();
+    for start_node in start_nodes {
+        let path_len = check_and_get_path_length(start_node, input);
+        path_lengths.push(path_len.expect("path does not have expected properties"));
+    }
+    return path_lengths.iter().fold(1usize, |a, b| lcm(a, *b));
+}
+
+fn check_and_get_path_length(start_node: &str, input: &Input) -> Option<usize> {
+    let (initial_end_node, count) = path_to_end(start_node, input);
+    let (loop_end_node, loop_count) = path_to_end(start_node, input);
+    // These are convenient properties of our input.
+    if loop_end_node != initial_end_node {
+        return None;
+    }
+    if count != loop_count {
+        return None;
+    }
+    return Some(count);
+}
+
+fn path_to_end<'a>(start: &'a str, input: &'a Input) -> (&'a str, usize) {
+    let mut count: usize = 0;
+    let mut current_node = start;
+    loop {
+        let direction = &input.directions[count % input.directions.len()];
+        current_node = advance_node(input, current_node, direction);
+        count += 1;
+        if current_node.ends_with("Z") {
+            return (current_node, count);
+        }
+    }
 }
 
 fn parse_input(text: &str) -> IResult<&str, Input> {
@@ -56,7 +100,7 @@ fn parse_input(text: &str) -> IResult<&str, Input> {
         graph.insert(label, node);
     }
 
-    return Ok(("", Input{directions, graph}));
+    return Ok(("", Input { directions, graph }));
 }
 
 fn directions(text: &str) -> IResult<&str, Vec<Direction>> {
