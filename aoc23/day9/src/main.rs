6d1bbc9b2389b32c53c487b9380cfae1b09ca5f5
Judson Powers <judson.powers@gmail.com>
2023-12-09 10:57:57 -0500

    Day 9, Part 1

---
+++ aoc23/day9/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use nom::character::complete::{i64 as pi64, line_ending, space1};
+use nom::IResult;
+use nom::multi::separated_list1;
+
+fn main() {
+    let input_s = fs::read_to_string("input.txt").unwrap();
+    let (_, input) = parse_input(&input_s).unwrap();
+    println!("Part one: {}", part_one(&input));
+}
+
+fn part_one(input: &Vec<Vec<i64>>) -> i64 {
+    input.iter().map(prediction).sum()
+}
+
+fn prediction(input: &Vec<i64>) -> i64 {
+    let mut seq = input.clone();
+    let mut result: i64 = *seq.last().unwrap();
+    let mut all_zero: bool = false;
+    while !all_zero {
+        all_zero = true;
+        for idx in 1..seq.len() {
+            seq[idx - 1] = seq[idx] - seq[idx - 1];
+            if seq[idx - 1] != 0 {
+                all_zero = false;
+            }
+        }
+        seq.pop();
+        result += seq.last().unwrap();
+    }
+    return result;
+}
+
+fn parse_input(input: &str) -> IResult<&str, Vec<Vec<i64>>>{
+    separated_list1(line_ending, separated_list1(space1, pi64))(input)
+}
