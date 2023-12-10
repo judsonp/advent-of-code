6a303b1a2125720e957e8996c97791d572a6eeb2
Judson Powers <judson.powers@gmail.com>
2023-12-10 11:38:05 -0500

    Day 10, Part 2

--- aoc23/day10/src/main.rs
+++ aoc23/day10/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use derive_more::Constructor;
 
@@ -18,6 +18,7 @@ fn main() {
     let input_s = fs::read_to_string("input.txt").unwrap();
     let input = parse_input(&input_s);
     println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
 }
 
 fn part_one(input: &Graph) -> u32 {
@@ -39,6 +40,70 @@ fn part_one(input: &Graph) -> u32 {
     println!("Loop size: {}", steps);
     return steps / 2;
 }
+
+fn part_two(input: &Graph) -> u32 {
+    let cage = find_loop(input);
+
+    let max_x = input.graph.keys().map(|p| p.x).max().unwrap();
+    let max_y = input.graph.keys().map(|p| p.y).max().unwrap();
+
+    let mut inside_cage: u32 = 0;
+
+    for x in 0..=max_x {
+        for y in 0..=max_y {
+            if is_inside_cage(&Point::new(x, y), &cage, input) {
+                inside_cage += 1;
+            }
+        }
+    }
+
+    return inside_cage;
+}
+
+fn is_inside_cage(point: &Point, cage: &HashSet<Point>, input: &Graph) -> bool {
+    if cage.contains(point) {
+        return false;
+    }
+    let cage_points_on_downright_diag =
+        cage.iter().filter(|p| p.x > point.x && (p.x - point.x == p.y - point.y));
+    let ignoring_bad_corners
+        = cage_points_on_downright_diag.filter(|p| !is_tangent_diagonal(p, input.graph.get(p).unwrap()));
+    let crossings = ignoring_bad_corners.count();
+    return crossings % 2 == 1;
+}
+
+fn is_tangent_diagonal(p: &Point, adj: &[Point; 2]) -> bool {
+    // 7 and L are ignorable
+    // This isn't general, but it's the way we constructed 7 and L
+    if adj[0] == Point::new(p.x, p.y - 1) && adj[1] == Point::new(p.x + 1, p.y) {
+        return true;
+    }
+    if adj[0] == Point::new(p.x, p.y + 1) && adj[1] == Point::new(p.x - 1, p.y) {
+        return true;
+    }
+    return false;
+}
+
+fn find_loop(input: &Graph) -> HashSet<Point> {
+    let mut cage: HashSet<Point> = HashSet::new();
+
+    let start = input.start;
+    let mut cur = input.graph.get(&start).unwrap()[0];
+    let mut last = start;
+
+    cage.insert(start);
+
+    while cur != start {
+        cage.insert(cur);
+        let next = input.graph.get(&cur).unwrap().iter()
+            .filter(|n| **n != last).next().unwrap();
+        last = cur;
+        cur = *next;
+    }
+
+    return cage;
+}
+
 
 fn parse_input(input: &str) -> Graph {
     let mut graph: HashMap<Point, [Point; 2]> = HashMap::new();
