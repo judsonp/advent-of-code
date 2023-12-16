18388cb772a92a3c591846d960e5fa960d5c8313
Judson Powers <judson.powers@gmail.com>
2023-12-16 15:30:09 -0500

    Day 13, Part 2

--- aoc23/day13/src/main.rs
+++ aoc23/day13/src/main.rs
@@ -17,11 +17,18 @@ fn main() {
     let input_s = fs::read_to_string("inputs/day13.txt").unwrap();
     let input = parse_input(&input_s);
     println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
 }
 
 fn part_one(input: &Vec<Field>) -> usize {
     input.iter()
         .map(|field| field_score(field))
+        .sum()
+}
+
+fn part_two(input: &Vec<Field>) -> usize {
+    input.iter()
+        .map(|field| smudged_field_score(field))
         .sum()
 }
 
@@ -35,6 +42,16 @@ fn field_score(field: &Field) -> usize {
     panic!("No reflection!");
 }
 
+fn smudged_field_score(field: &Field) -> usize {
+    if let Some(h) = find_smudged_reflect(&field.cols) {
+        return h;
+    }
+    if let Some(v) = find_smudged_reflect(&field.rows) {
+        return 100 * v;
+    }
+    panic!("No reflection!");
+}
+
 fn find_reflect(elems: &Vec<u64>) -> Option<usize> {
     (1..elems.len()).filter(|idx| has_reflect(elems, *idx)).next()
 }
@@ -43,6 +60,20 @@ fn has_reflect(elems: &Vec<u64>, idx: usize) -> bool {
     let left = (0..idx).rev();
     let right = idx..elems.len();
     left.zip(right).all(|(i1, i2)| elems[i1] == elems[i2])
+}
+
+fn find_smudged_reflect(elems: &Vec<u64>) -> Option<usize> {
+    (1..elems.len()).filter(|idx| has_smudged_reflect(elems, *idx)).next()
+}
+
+fn has_smudged_reflect(elems: &Vec<u64>, idx: usize) -> bool {
+    let left = (0..idx).rev();
+    let right = idx..elems.len();
+    let diffcount = left.zip(right)
+        .map(|(i1, i2)| elems[i1] ^ elems[i2])
+        .map(|x| x.count_ones())
+        .sum::<u32>();
+    return diffcount == 1;
 }
 
 fn parse_input(input: &str) -> Vec<Field> {
@@ -84,5 +115,12 @@ mod tests {
         let input_s = fs::read_to_string("../examples/day13.txt").unwrap();
         let input = parse_input(&input_s);
         assert_eq!(part_one(&input), 405);
+    }
+
+    #[test]
+    fn example_part2() {
+        let input_s = fs::read_to_string("../examples/day13.txt").unwrap();
+        let input = parse_input(&input_s);
+        assert_eq!(part_two(&input), 400);
     }
 }
